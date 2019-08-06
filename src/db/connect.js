32e56dfd993b4629f1f6d15b17507713f0596023
alberttjin <albertjin@berkeley.edu>
2019-08-06 10:28:33 -0400

    changed mongo url

--- src/db/connect.js
+++ src/db/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URL || 'mongodb+srv://ajin:JUssiSVl0LBF7tnZ@cluster0-ggiav.mongodb.net/anime-db?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/anikeepserver', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: true
